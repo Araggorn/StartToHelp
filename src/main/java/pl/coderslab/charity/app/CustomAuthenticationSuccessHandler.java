3836b669277612050ceb16bb91a19ae3022e8dfe
Araggorn <zerelik@gmail.com>
2021-01-13 17:27:37 +0100

    optimalized CustomAuthSuccHandler

--- src/main/java/pl/coderslab/charity/app/CustomAuthenticationSuccessHandler.java
+++ src/main/java/pl/coderslab/charity/app/CustomAuthenticationSuccessHandler.java
@@ -1,11 +1,9 @@
 package pl.coderslab.charity.app;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import pl.coderslab.charity.service.SingleUserService;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -16,17 +14,10 @@ import java.util.Set;
 @Configuration
 public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
 
-    private SingleUserService usersService;
-
-    @Autowired
-    public void setUsersService(SingleUserService userService) {
-        this.usersService = userService;
-    }
-
     @Override
     public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
 
-       // Long userId = usersService.findUserIdByEmail(SecurityUtils.username());
+        // Long userId = usersService.findUserIdByEmail(SecurityUtils.username());
         Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
 
         if (roles.contains("ROLE_ADMIN")) {
